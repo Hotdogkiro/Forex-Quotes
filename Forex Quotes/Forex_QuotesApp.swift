2598ddb1c35df9fd3632f6418567ebad555ae50b
Dominik Helbling <dominik.helbling@ubs.com>
2021-11-24 19:16:45 +0100

    Initial Commit

---
+++ Forex Quotes/Forex_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forex_QuotesApp.swift
+//  Forex Quotes
+//
+//  Created by Dominik Helbling on 24.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Forex_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
